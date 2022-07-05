224f3cf56aa077907f27e983e23855ce0dc41822
stevenmanriqueca <stevenmnrq@gmail.com>
2022-07-05 08:18:50 -0500

    Added validations with express validator in Notes and Entries Routes

--- routes/entries.ts
+++ routes/entries.ts
@@ -1,19 +1,40 @@
 import { Router } from "express";
+import { check } from "express-validator";
 import {
 	deleteEntry,
 	getEntries,
 	newEntry,
 	updateEntry,
 } from "../controllers/entries";
-import {validateJWT} from "../middlewares";
+import { validateJWT } from "../middlewares";
 
 const router = Router();
 
-router.use(validateJWT)
+router.use(validateJWT);
 
 router.get("/", getEntries);
-router.post("/", newEntry);
-router.put("/:idEntry", updateEntry);
+router.post(
+	"/",
+	[
+		check("title", "Title is required").not().isEmpty(),
+		check("description", "Description is required").not().isEmpty(),
+		check("priority", "Priority is required").not().isEmpty(),
+		check("colorEntry", "ColorEntry is required").not().isEmpty(),
+		check("status", "status is required").not().isEmpty(),
+	],
+	newEntry
+);
+router.put(
+	"/:idEntry",
+	[
+		check("title", "Title is required").not().isEmpty(),
+		check("description", "Description is required").not().isEmpty(),
+		check("priority", "Priority is required").not().isEmpty(),
+		check("colorEntry", "ColorEntry is required").not().isEmpty(),
+		check("status", "status is required").not().isEmpty(),
+	],
+	updateEntry
+);
 router.delete("/:idEntry", deleteEntry);
 
 export default router;
--- routes/notes.ts
+++ routes/notes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { check } from "express-validator";
 import {
 	deleteNote,
 	getNotes,
@@ -12,8 +13,24 @@ const router = Router();
 router.use(validateJWT);
 
 router.get("/", getNotes);
-router.post("/", newNote);
-router.put("/:idNote", updateNote);
+router.post(
+	"/",
+	[
+		check("title", "Title is required").not().isEmpty(),
+		check("description", "Description is required").not().isEmpty(),
+		check("colorEntry", "ColorEntry is required").not().isEmpty(),
+	],
+	newNote
+);
+router.put(
+	"/:idNote",
+	[
+		check("title", "Title is required").not().isEmpty(),
+		check("description", "Description is required").not().isEmpty(),
+		check("colorEntry", "ColorEntry is required").not().isEmpty(),
+	],
+	updateNote
+);
 router.delete("/:idNote", deleteNote);
 
 export default router;
